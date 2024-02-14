57774b46985095e1bc7e99ab800bdb5004f55feb
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-02-14 22:59:31 +0900

    Create sample project

---
+++ Concurrency/Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by 桂川 祐介 on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
