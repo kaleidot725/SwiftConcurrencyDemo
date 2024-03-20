d9ce7c5c8286908f34144a8527fe180ef955b185
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-03-20 09:41:30 +0900

    Create AsyncSequence Demo

---
+++ AsyncSequence/AsyncSequence/AsyncSequenceApp.swift
@@ -0,0 +1,14 @@
+//
+// AsyncSequenceApp.swift
+  
+
+import SwiftUI
+
+@main
+struct AsyncSequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
