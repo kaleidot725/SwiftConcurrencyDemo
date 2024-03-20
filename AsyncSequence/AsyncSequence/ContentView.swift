4609b31c16e675a6131444f3db5993ce686756f8
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-03-20 14:15:29 +0900

    Update

--- AsyncSequence/AsyncSequence/ContentView.swift
+++ AsyncSequence/AsyncSequence/ContentView.swift
@@ -1,18 +1,11 @@
 //
 // ContentView.swift
-  
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        UserAPIsView()
     }
 }
 
